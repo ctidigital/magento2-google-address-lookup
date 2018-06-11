68ad404f7c0db4f1f8ef76ea6ae429ce2db62531
Jonathan Day <jonathan@aligent.com.au>
2018-06-11 17:40:46 +0930

    updates to display region and region_id, use JS to set the region_id to valid Magento ID-based value, also merge the street_number and "route" into Address Line 1 as per Australian standard

--- view/src/frontend/web/js/model/address/addressData.js
+++ view/src/frontend/web/js/model/address/addressData.js
@@ -45,7 +45,7 @@ define(['ko'], function (ko) {
             street_number: 'short_name',
             route: 'long_name',
             locality: 'long_name',
-            administrative_area_level_1: 'short_name',
+            administrative_area_level_1: 'long_name',
             country: 'long_name',
             postal_code: 'long_name',
             postal_town: 'long_name',
